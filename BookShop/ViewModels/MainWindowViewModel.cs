54fda41ec3a2f64b5cb592a1b39c29a73813226d
EBuravskiy <elric0sphinx@gmail.com>
2025-11-19 16:39:47 +0500

    Add MainWindow view model and View Model locator

---
+++ BookShop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string title = "Главное окно програмы";
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+            }
+        }
+    }
+}
